0168c999339954f86efa849e21ddd6b3c50ab3ba
ryanalonzo <r_alonzo@outlook.com>
2017-12-12 15:56:50 +0800

    hello

--- src/index.php
+++ src/index.php
@@ -5,4 +5,4 @@ require_once('../vendor/autoload.php');
 $forecaster = new Forecaster(new ApixuApi("c58d4914f6e643c3ba481924170408"));
 $forecast = $forecaster->getForecast('Quezon City');
 
-echo "The temperature today in {$forecast['city']} is {$forecast['temp_c']}";
+echo "The temperature today in {$forecast['city']} is {$forecast['temp_c']} HAHAHAA";
